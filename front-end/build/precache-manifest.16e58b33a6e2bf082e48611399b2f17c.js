47ca64a19fc9a366ca1a90331b87921f1b5bcd4e
Cicada1105 <42916703+Cicada1105@users.noreply.github.com>
2019-10-15 20:11:16 -0500

    Add files via upload
    This is a test build to see if building the react app under deployment circumstances will allow it to be run locally after cloning

---
+++ front-end/build/precache-manifest.16e58b33a6e2bf082e48611399b2f17c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "553ca0e4a330661985e325525dea8e8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b35a3cd98a291520f0b7",
+    "url": "/static/css/main.ebc213ce.chunk.css"
+  },
+  {
+    "revision": "e467be23ef54153e8e8d",
+    "url": "/static/js/2.553ceddc.chunk.js"
+  },
+  {
+    "revision": "b35a3cd98a291520f0b7",
+    "url": "/static/js/main.d0a08cee.chunk.js"
+  },
+  {
+    "revision": "a2a63635617655cc8f52",
+    "url": "/static/js/runtime-main.7efd5a7d.js"
+  }
+]);
